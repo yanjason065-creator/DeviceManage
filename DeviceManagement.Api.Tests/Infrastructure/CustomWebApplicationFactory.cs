29b2fa2808d52607602957e870e2b8b05073e2ab
JasonYan1985 <yanjason065@outlook.com>
2026-07-25 17:01:51 -0400

    Update Test Database source

--- DeviceManagement.Api.Tests/Infrastructure/CustomWebApplicationFactory.cs
+++ DeviceManagement.Api.Tests/Infrastructure/CustomWebApplicationFactory.cs
@@ -2,6 +2,7 @@
 using DeviceManagement.Api.Tests.Helpers;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
@@ -17,42 +18,39 @@ namespace DeviceManagement.Api.Tests.Infrastructure
    public class CustomWebApplicationFactory:
         WebApplicationFactory<Program>
     {
-        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        private readonly SqliteConnection _connection;
+
+        public CustomWebApplicationFactory()
+        {
+            _connection = new SqliteConnection("DataSource=:memory:");
+            _connection.Open();
+        }
+        protected override void ConfigureWebHost(
+       IWebHostBuilder builder)
         {
             builder.UseEnvironment("Test");
 
-            //builder.ConfigureServices(services =>
-            //{
-            //    var descriptor =
-            //        services.SingleOrDefault(
-            //            d =>
-            //            d.ServiceType ==
-            //            typeof(DbContextOptions<AppDbContext>)
-            //         );
 
-            //    if (descriptor != null)
-            //    {
-            //        services.Remove(descriptor);
-            //    }
+            builder.ConfigureServices(services =>
+            {
+                services.RemoveAll<
+                    DbContextOptions<AppDbContext>>();
 
-            //    services.RemoveAll<AppDbContext>();
 
-            //    services.RemoveAll<DbContextOptions<AppDbContext>>();
+                services.AddDbContext<AppDbContext>(
+                    options =>
+                    {
+                        options.UseSqlite(_connection);
+                    });
 
-            //    services.AddDbContext<AppDbContext>(
-            //        options =>
-            //        {
-            //            options.UseSqlite(
 
-            //                "DataSource=:memory:"
-            //             );
-            //        });
-            //});
+                var serviceProvider =
+                    services.BuildServiceProvider();
 
-            //using(var scope = Services.CreateScope())
-            //{
-            //    TestDatabaseInitializer.Initialize(Services);
-            //}
+
+                TestDatabaseInitializer.Initialize(
+                    serviceProvider);
+            });
         }
     }
 }
